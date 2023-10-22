aa3eb63dd7e8ca89665ff2bb671129f4164bcb2f
FrederikBA <frederikbandersen98@gmail.com>
2023-10-22 15:24:41 +0200

    refactor program.cs async

--- CamundaService2/Program.cs
+++ CamundaService2/Program.cs
@@ -5,34 +5,25 @@ using CamundaService2.Models.Dtos;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-namespace CamundaService2;
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = factory.CreateConnection();
+using var channel = connection.CreateChannel();
 
-class Program
+channel.QueueDeclare("camundaExternalTask");
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += async (model, eventArgs) =>
 {
-    static async Task Main(string[] args)
-    {
-        
-        var factory = new ConnectionFactory { HostName = "localhost" };
-        var connection = factory.CreateConnection();
-        using var channel = connection.CreateChannel();
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var dto = JsonSerializer.Deserialize<CompleteExternalTaskDto>(message);
 
-        channel.QueueDeclare("camundaExternalTask");
-        Console.WriteLine(" [*] Waiting for messages.");
+    //Complete External Task
+    var camundaTask = new CamundaExternalTask();
+    await camundaTask.CompleteExternalTask(dto);
+};
 
-        var consumer = new EventingBasicConsumer(channel);
-        consumer.Received += async (model, eventArgs) =>
-        {
-            var body = eventArgs.Body.ToArray();
-            var message = Encoding.UTF8.GetString(body);
-            var dto = JsonSerializer.Deserialize<CompleteExternalTaskDto>(message);
-    
-            //Complete External Task
-            var camundaTask = new CamundaExternalTask();
-            await camundaTask.CompleteExternalTask(dto);
-        };
-
-        channel.BasicConsume(queue: "camundaExternalTask", autoAck: true, consumer: consumer);
-        Console.WriteLine(" Press [enter] to exit.");
-        Console.ReadKey();
-    }
-}
+channel.BasicConsume(queue: "camundaExternalTask", autoAck: true, consumer: consumer);
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadKey();
